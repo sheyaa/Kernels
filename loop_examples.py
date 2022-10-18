65c052a2a54aa1a3d1e0f0e546dd159392b9a313
Yodd Mena <taekidoyodd@live.com>
2022-10-18 14:59:18 -0700

    Update loop_examples.py
    made changes in for loops

--- loop_examples.py
+++ loop_examples.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+#centers
 centers = [
     np.array([0,1]),
     np.array([2,0]),
@@ -9,7 +9,7 @@ weights = [1, 3, -2]
 
 #for c in centers:
 #    print(c)
-
+'''
 for i in range( len(centers) ):
     print(centers[i])
     print(weights[i])
@@ -27,21 +27,21 @@ for i in range( len(centers) ):
 
 S=0
 for j in range( len(weights) ):
-    S=S + (weights[j])
+    #S=S + (weights[j])
     
 print(S)
-
+'''
 
 ###############################################
 from numpy import linalg as LA
-
+#x array
 x = [
      np.array([1,1]),
      np.array([0,2]),
      np.array([2,0]),
      np.array([2,1])]
 
-for i in range(len(centers)):
-    for k in range(len(x)):
-        D = LA.norm (x[0] - centers[i])
-print("D =", D)
+for k in range(len(x)): #for each x 
+    for i in range(len(centers)):  #for each center
+        D = LA.norm (x[k] - centers[i])
+    print("D =", D)
