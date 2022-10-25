3cfd5fdeed5f38230e9830672e07bbb62af8ce70
Yodd Mena <taekidoyodd@live.com>
2022-10-25 15:02:07 -0700

    updating function

--- loop_examples.py
+++ loop_examples.py
@@ -1,13 +1,59 @@
 import numpy as np
-#centers
-centers = [
-    np.array([0,1]),
-    np.array([2,0]),
-    np.array([2,2])
-    ]
-weights = [1, 3, -2]
+from numpy import linalg as LA
+###############################################
+#DEFINE FUNC
+# np.piecewise?
+def phi(x, T=1):
+    result = np.piecewise(
+        x,
+        [x <= T, x>T],
+        [1, 0]
+        )
+    return result
 
-#for c in centers:
+#x array
+x = [
+     np.array([1,1]),
+     np.array([0,2]),
+     np.array([2,0]),
+     np.array([2,1])]
+
+#centers
+c = [
+     np.array([0,1]),
+     np.array([2,0]),
+     np.array([2,2])]
+
+
+#weights
+w= np.array([1, 3, -2])
+
+y = np.zeros(len(x))  #for each center for i
+
+
+for k in range(len(x)): #for each x 
+    for i in range(len(c)):  #for each center
+        D = LA.norm (x[k] - c[i])
+        
+        y[k] = y[k] + (w[i] * phi(D))
+        
+        print("D =", D)
+    print("y=", y)
+    print("k=", k)
+   # print("center =", c[i])
+#print("x-value =", x[k])
+        
+ 
+
+
+ 
+    
+ 
+    
+ 
+    
+############################################### 
+    #for c in centers:
 #    print(c)
 '''
 for i in range( len(centers) ):
@@ -31,17 +77,3 @@ for j in range( len(weights) ):
     
 print(S)
 '''
-
-###############################################
-from numpy import linalg as LA
-#x array
-x = [
-     np.array([1,1]),
-     np.array([0,2]),
-     np.array([2,0]),
-     np.array([2,1])]
-
-for k in range(len(x)): #for each x 
-    for i in range(len(centers)):  #for each center
-        D = LA.norm (x[k] - centers[i])
-    print("D =", D)
